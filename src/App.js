9406e31856cb03099d00fb89ca7e0e849c664961
Wania Khan <waniak305@gmail.com>
2024-03-16 07:36:30 +0500

    upd

--- src/App.js
+++ src/App.js
@@ -11,34 +11,37 @@ function App() {
     </div>
     <br></br><br></br><br></br>
     <br></br><br></br><br></br>
+    
     <div className='div2'>
     <p className='p1'>Hi, I am John, Creative Technologist</p>
     <p className='p2'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-      <img className='img1' src={img1} alt='error!'/>
-      <br></br>
-      <button
-  className='btn1'
-  style={{
-    borderRadius: '2px',
-    border: 'solid 2px #FF6464',
-    background: '#FF6464',
-    color: '#FFFFFF',
-    width: '208px',
-    height: '47px',
-    marginTop: '-220px',
-    marginLeft: '10%',
-    marginRight: '90%'
-  }}
->Download Resume</button>
+       <img className='img1' src={img1} alt='error!'/>
     </div>
-    <br></br><br></br><br></br><br></br>
-    <div className='div3'>
-      
-      <span className='sp4'>View All</span>
-    <div className='div3a'>
+    <button className='btn1' style={{
+      borderRadius: '2px',
+      border: 'solid 2px #FF6464',
+      background: '#FF6464',
+      color: '#FFFFFF',
+      width: '208px',
+      height: '47px',
+      display: 'flex',
+      justifyContent: 'center', 
+      alignItems: 'center',
+      marginLeft: '10%'
+      }}>Download Resume</button>
+    <br></br><br></br><br></br><br></br><br></br>
+    <div className='div3' style={{width: '100%'}}>
+      <span className='sp4'>Recent Posts</span>
+      <span className='sp5'>View All</span>
+      <div className='div3a'>
+      <div className='3a.1'>
+      Making a design system from scratch<br></br>
+      12 Feb 2020
 
+      </div>
+      <div className='3a.2'></div>
     </div>
-    </div>
+    </div>  
     </div>
   );
 }
