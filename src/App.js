2afb35f80aff8a1e62ea0881af6a0c8628913443
Wania Khan <waniak305@gmail.com>
2024-03-18 23:49:11 +0500

    upd

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
   {/* ------First Section------ */}
   <div className='container1'>
-    <div className='container1a'><span className='sp1'><b>Works&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></span>
-  <span className='sp2'><b>Blog&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></span>
-  <span className='sp3'><b>Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></span></div>
+    <div className='container1a'><span className='sp1'>Works&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+  <span className='sp2'>Blog&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+  <span className='sp3'>Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></div>
   </div>
   <br></br>
   {/* ------Second Section------ */}
@@ -30,8 +30,8 @@ function App() {
       <span className='sp5'>View All</span>
       </div>
     <div className='content-container'>
-    <div className='left-block1'>
-    <span className='sp6'>Making a design system from scratch</span><br></br><br></br>
+    <div className='block'>
+    <span className='sp6' style={{textAlign: 'left'}}>Making a design system from scratch</span><br></br><br></br>
       <span className='sp7'><span className='sp7a'>12 Feb 2020 </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <span className='sp7b'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <span className='sp7c'>Design, Pattern </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -39,8 +39,8 @@ function App() {
       <span className='sp8'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
        Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
     </div>
-    <div className='right-block1'>
-
+    <div className='block'>
+      Block2
     </div>
     </div>
     
