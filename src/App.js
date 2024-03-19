cc04103931e71f768894612f2fddbbe4404ef901
Wania Khan <waniak305@gmail.com>
2024-03-19 22:44:12 +0500

    upd

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import img1 from './imgs/img1.png';
 import img2 from './imgs/img2.png';
 import img3 from './imgs/img3.png';
+import img4 from './imgs/img4.png';
+
 function App() {
   return (
     <div className="App">
@@ -70,21 +72,42 @@ function App() {
     </div>
     </div>
     <hr className='hr'></hr>
+    <br></br>
     <div className='container5'>
-    <div className='box1'>
+    <div className='box3'>
     <img className='img3' src={img3} alt='error!'/>
     </div>
-    <div className='box2'>
-     <span className='sp12'>Vibrant Portraits of 2020</span>
+    <div className='box4'>
+     <span className='sp15'>Vibrant Portraits of 2020</span>
      <br></br><br></br>
      <span className='disc'>2018</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-     <span className='sp13'>Illustrator</span>
+     <span className='sp16'>Illustration</span>
      <br></br>
-     <span className='sp14'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+     <span className='sp17'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
        Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
     </div>
     </div>
     <hr className='hr'></hr>
+    <div className='container6'>
+    <div className='box5'>
+    <img className='img4' src={img4} alt='error!'/>
+    </div>
+    <div className='box6'>
+     <span className='sp18'>Vibrant Portraits of 2020</span>
+     <br></br><br></br>
+     <span className='disc'>2018</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     <span className='sp19'>Illustration</span>
+     <br></br>
+     <span className='sp20'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+       Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
+    </div>
+    </div>
+    <hr className='hr'></hr>
+    <br></br>
+    {/* ------Fifth Section------ */}
+    <div className='container7'>
+
+    </div>
   </div>
   );
 }
