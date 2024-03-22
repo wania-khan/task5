ace751b92279a1ea1e1e7f45fdde15cc95f121b4
Wania Khan <waniak305@gmail.com>
2024-03-22 22:48:18 +0500

    upd folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import Hero from './components/hero.js';
+import Hero1 from './components/hero1.js';
 import img2 from './imgs/img2.png';
 import img3 from './imgs/img3.png';
 import img4 from './imgs/img4.png';
@@ -14,38 +15,13 @@ function App() {
   <span className='sp2'>Blog&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
   <span className='sp3'>Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></div>
   </div>
+  <br></br><br></br>
   {/* ------Second Section------ */}
-  <Hero></Hero>
+  <Hero/>
+  <br></br><br></br>
   {/* ------Third Section------ */}
   <div className='container3'>
-  <div className='div33'>
-    <div className='inner-container'>
-      <div className='fluid-container'>
-      <span className='sp4'>Recent Posts</span>
-      <span className='sp5'>View All</span>
-      </div>
-        </div>
-      </div>
-    <div className='content-container'>
-    <div className='block'>
-    <span className='sp6' style={{textAlign: 'left'}}>Making a design system from scratch</span><br></br><br></br>
-      <span className='sp7'><span className='sp7a'>12 Feb 2020 </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <span className='sp7b'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <span className='sp7c'>Design, Pattern </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      </span><br></br><br></br>
-      <span className='sp8'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
-       Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
-    </div>
-    <div className='block'>
-    <span className='sp9' style={{textAlign: 'left'}}>Creating pixel perfect icons in Figma</span><br></br><br></br>
-      <span className='sp10'><span className='sp10a'>12 Feb 2020 </span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <span className='sp10b'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <span className='sp10c'>Figma, Icon Design</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      </span><br></br><br></br>
-      <span className='sp11'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
-       Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
-    </div>
-    </div>
+   <Hero1/>
    </div>
    <p className='p3'  style={{textAlign: 'left'}}>Featured works</p>
     {/* ------Fourth Section------ */}
