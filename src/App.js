53905b72ffdb88ab454847f0f5c81a971954f2ea
Wania Khan <waniak305@gmail.com>
2024-03-24 00:19:23 +0500

    upd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import React from 'react';
-import Hero from './components/hero.js';
-import Hero1 from './components/hero1.js';
-import img2 from './imgs/img2.png';
-import img3 from './imgs/img3.png';
-import img4 from './imgs/img4.png';
+import Hero from './assets/components/hero.js';
+import Hero1 from './assets/components/hero1.js';
+import img2 from './assets/imgs/img2.png';
+import img3 from './assets/imgs/img3.png';
+import img4 from './assets/imgs/img4.png';
 
 function App() {
   return (
@@ -21,56 +21,11 @@ function App() {
   <br></br><br></br>
   {/* ------Third Section------ */}
   <div className='container3'>
-   <Hero1/>
-   </div>
+  <Hero1/>
+  </div>
    <p className='p3'  style={{textAlign: 'left'}}>Featured works</p>
     {/* ------Fourth Section------ */}
-    <div className='container4'>
-    <div className='box1'>
-    <img className='img2' src={img2} alt='error!'/>
-    </div>
-    <div className='box2'>
-     <span className='sp12'>Designing Dashboards</span>
-     <br></br><br></br>
-     <span className='disc'>2020</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-     <span className='sp13'>Dashboard</span>
-     <br></br>
-     <span className='sp14'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
-       Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
-    </div>
-    </div>
-    <hr className='hr'></hr>
-    <br></br>
-    <div className='container5'>
-    <div className='box3'>
-    <img className='img3' src={img3} alt='error!'/>
-    </div>
-    <div className='box4'>
-     <span className='sp15'>Vibrant Portraits of 2020</span>
-     <br></br><br></br>
-     <span className='disc'>2018</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-     <span className='sp16'>Illustration</span>
-     <br></br>
-     <span className='sp17'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
-       Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
-    </div>
-    </div>
-    <hr className='hr'></hr>
-    <div className='container6'>
-    <div className='box5'>
-    <img className='img4' src={img4} alt='error!'/>
-    </div>
-    <div className='box6'>
-     <span className='sp18'>36 Days of Malayalam type</span>
-     <br></br><br></br>
-     <span className='disc'>2018</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-     <span className='sp19'>Typography</span>
-     <br></br>
-     <span className='sp20'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
-       Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
-    </div>
-    </div>
-    <hr className='hr'></hr>
+    
     <br></br><br></br><br></br>
     {/* ------Fifth Section------ */}
     <div className='container7'>
---
+++ src/assets/components/hero1.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Hero1(){
+    return(
+        <>
+      <div className='container3a'>
+      <div className='row-container'>
+      <span className='sp4'>Recent Posts</span>
+      <span className='sp5'>View All</span>
+      </div>
+      <br></br>
+      <div className='coloumn-container'>
+        <div className='block'>
+        <span className='sp6'>Making a design system from scratch</span>
+        <br></br><br></br><br></br>
+        <span className='sp7'>12 Feb 2020&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Design, Pattern</span>
+        <br></br><br></br><br></br>
+        <span className='sp8'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+       Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
+        </div>
+        <div className='block'>
+          <span className='sp6'>Creating pixel perfect icons in Figma</span>
+          <br></br><br></br><br></br>
+          <span className='sp7'>12 Feb 2020&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;Figma, Icon Design</span>
+          <br></br><br></br><br></br>
+          <span className='sp8'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+       Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
+        </div>
+      </div>
+      </div>
+        </>
+    );
+}
+
+export default Hero1;
